83624e42bd1818d0aac96b313ef0483e598f4fff
greyhoundforty <ryantiffany@fastmail.com>
2020-07-01 19:10:11 -0500

    Inital commit
    Signed-off-by: greyhoundforty <ryantiffany@fastmail.com>

---
+++ main.tf
@@ -0,0 +1,19 @@
+data "ibm_compute_ssh_key" "deploymentKey" {
+  label = var.ssh_key
+}
+
+resource "ibm_compute_vm_instance" "node" {
+  hostname             = var.hostname
+  domain               = var.domain
+  os_reference_code    = var.os_image
+  datacenter           = var.datacenter
+  network_speed        = 1000
+  hourly_billing       = true
+  private_network_only = false
+  local_disk           = true
+  user_metadata        = file("installer.sh")
+  flavor_key_name      = var.flavor
+  tags                 = [var.datacenter]
+  ssh_key_ids          = [data.ibm_compute_ssh_key.deploymentKey.id]
+}
+
---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "iaas_classic_username" {
+  decription = "IBM Cloud IaaS Username."
+  type       = string
+  default    = ""
+}
+
+variable "iaas_classic_api_key" {
+  decription = "IBM Cloud IaaS User API key."
+  type       = string
+  default    = ""
+}
+
+variable "os_image" {
+  description = "Default operating system image for compute instance."
+  type        = string
+  default     = "UBUNTU_18_64"
+}
+
+variable "flavor" {
+  description = "Default instance size."
+  type        = string
+  default     = "BL2_2X4X100"
+}
+
+variable "domain" {
+  type    = string
+  default = "cdetesting.com"
+}
+
+variable "datacenter" {
+  decription = "Datacenter where instance will be deployed."
+  type       = string
+  default    = ""
+}
+
+variable "ssh_key" {
+  decription = "Classic IaaS ssh key to add to compute instance."
+  type       = string
+  default    = ""
+}
+
+variable "hostname" {
+  decription = "Hostname for instance."
+  type       = string
+  default    = ""
+}
